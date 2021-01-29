668a68bbdd5a3a965aebef1a433c85198f13afd1
robertvala <robavala@espol.edu.ec>
2021-01-29 13:06:16 -0500

    cambio de nombre registros a resultados

--- app/src/main/java/com/amst/askhero/Resultado.java
+++ app/src/main/java/com/amst/askhero/Resultado.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Registros extends AppCompatActivity {
+public class Resultado extends AppCompatActivity {
 
 
 }
