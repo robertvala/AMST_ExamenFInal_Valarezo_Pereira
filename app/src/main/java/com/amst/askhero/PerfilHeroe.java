114997b426af467ff81aeb8658ddcbccb0a1b498
ppereira123 <52977467+ppereira123@users.noreply.github.com>
2021-01-29 13:54:38 -0500

    perfilHeroe

--- app/src/main/java/com/amst/askhero/PerfilHeroe.java
+++ app/src/main/java/com/amst/askhero/PerfilHeroe.java
@@ -2,7 +2,18 @@ package com.amst.askhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PerfilHeroe extends AppCompatActivity {
 
@@ -10,5 +21,33 @@ public class PerfilHeroe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_heroe);
+        Bundle bundleR=getIntent().getExtras();
+        TextView tvSup=findViewById(R.id.nombreHeroe);
+        TextView tvNombre=findViewById(R.id.nombreCompleto);
+        SuperHeroe superHeroe=(SuperHeroe)bundleR.getSerializable("Superheroes");
+        tvSup.setText(tvSup.getText()+" "+superHeroe.nombre);
+        tvNombre.setText(tvNombre.getText()+" "+superHeroe.alterego);
+        HashMap habilidades=superHeroe.habilidades;
+        int inteligencia=Integer.parseInt(habilidades.get("Inteligencia").toString());
+        int fuerza=Integer.parseInt(habilidades.get("Fuerza").toString());
+        int durabilidad=Integer.parseInt(habilidades.get("Durabilidad").toString());
+        int velocidad=Integer.parseInt(habilidades.get("Velocidad").toString());
+        int poder=Integer.parseInt(habilidades.get("Poder").toString());
+        int combate=Integer.parseInt(habilidades.get("Combate").toString());
+        BarChart barChart=findViewById(R.id.Grafico);
+        ArrayList <BarEntry> datos=new ArrayList<>();
+        datos.add(new BarEntry(1,inteligencia));
+        datos.add(new BarEntry(2,fuerza));
+        datos.add(new BarEntry(3,durabilidad));
+        datos.add(new BarEntry(4,velocidad));
+        datos.add(new BarEntry(5,poder));
+        datos.add(new BarEntry(6,combate));
+        BarDataSet barDataSet=new BarDataSet(datos,"Estadisticas");
+        BarData barData=new BarData(barDataSet);
+        barChart.setData(barData);
+    }
+    public void Volver(View v){
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
